cc8671c3576d70fc1bd1723d1e9e35572b21feb2
April <liska.april@gmail.com>
2026-07-21 21:01:23 -0400

    Python Syntax

--- basics.py
+++ basics.py
@@ -8,3 +8,18 @@ print(10*2)
 
 #Get Started
 print(sys.version)
+
+#Python Syntax
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5 # Integer variable
+y = "Hello, World!" # String variable
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
