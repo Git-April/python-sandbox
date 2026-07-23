5bb6a5adde2fbae59a44fd2c72986dc7db8c73d9
April <liska.april@gmail.com>
2026-07-22 20:59:25 -0400

    Python Variables and add notes

--- basics.py
+++ basics.py
@@ -70,3 +70,121 @@ This is
 a multiline
 comment
 """
+
+#Python Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4
+x = "Sally"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = "John"
+x = 'John'
+
+x = 3
+print(type(x))
+
+a = 4
+A = "Sally" #new
+print(a)
+print(A)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+myVariableName = "John"
+MyVariableName = "John"
+my_variable_name = "John"
+
+x, y, z = "Orange", "Banana", "Cherry" #new
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange" #new
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits #new
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x,y,z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+x = "awesome"
+
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc2():
+    x = "fantastic" #new
+    print("Python is " + x)
+
+myfunc2()
+
+print("Python is " + x)
+
+def myfunc3():
+    global myfunc3variable #new
+    myfunc3variable = "fantastic"
+
+myfunc3()
+
+print("Python is " + myfunc3variable)
+
+myfunc4variable = "awesome"
+
+def myfunc4():
+    global myfunc4variable
+    myfunc4variable = "fantastic"
+
+myfunc4()
+
+print("Python is " + myfunc4variable)
+
+x = 5
+y = "John"
+print(type(x))
