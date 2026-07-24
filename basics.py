a58c197a835dde63a0d4a6ef550ce337f3caf717
April <liska.april@gmail.com>
2026-07-23 20:10:08 -0400

    Python Numbers

--- basics.py
+++ basics.py
@@ -1,5 +1,5 @@
-#Get Started
-import sys
+import sys #Get Started
+import random #Python Numbers
 
 #Intro
 print("Hello, World!")
@@ -245,3 +245,54 @@ z = "Hello"
 print(type(x))
 print(type(y))
 print(type(z))
+
+#Python Numbers
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 2.8
+z = 1j
+a = float(x)
+b = int(y)
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(random.randrange(1, 10))
