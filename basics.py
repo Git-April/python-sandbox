1407d270fc8815e0209204f565533c6022574c2b
April <liska.april@gmail.com>
2026-07-25 19:12:23 -0400

    Python While Loops

--- basics.py
+++ basics.py
@@ -1788,3 +1788,37 @@ match day:
         print("Wednesday")
     case _:
         print("Other day")
+
+#Python While Loops
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+i = 0
+while i < 6:
+    i += 1
+    if i ==3:
+        continue
+    print(i)
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
