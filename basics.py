1631896602acc00c5abf90cb70550e2e44826831
April <liska.april@gmail.com>
2026-07-25 17:06:18 -0400

    Python Dictionaries

--- basics.py
+++ basics.py
@@ -1160,3 +1160,289 @@ print(colors)
 colors.add("yellow")
 colors.discard("green")
 print(len(colors))
+
+#Python Dictionaries
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict["brand"])
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020
+}
+print(thisdict)
+
+print(len(thisdict))
+
+thisdict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
+}
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(type(thisdict))
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = thisdict["model"]
+print(x)
+x = thisdict.get("model")
+print(x)
+
+x = thisdict.keys()
+print(x)
+print(type(x))
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = car.keys()
+print(x)
+car["color"] = "white"
+print(x)
+
+x = thisdict.values()
+print(x)
+print(type(x))
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+x = car.values()
+print(x)
+car["year"] = 2020
+print(x)
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+x = car.values()
+print(x)
+car["color"] = "red"
+print(x)
+
+x = car.items()
+print(x)
+print(type(x))
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+x = car.items()
+print(x)
+car["year"] = 2020
+print(x)
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+x = car.items()
+print(x)
+car["color"] = "red"
+print(x)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+if "model" in thisdict:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict["year"] = 2018
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.update({"year": 2020})
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.update({"color": "red"})
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+del thisdict
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+for x in thisdict:
+    print(x)
+
+for x in thisdict:
+    print(thisdict[x])
+
+for x in thisdict.values():
+    print(x)
+
+for x, y in thisdict.items():
+    print(x, y)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+myfamily = {
+    "child1" : {
+        "name": "Emil",
+        "year": 2004
+    },
+    "child2": {
+        "name": "Tobias",
+        "year": 2007
+    },
+    "child3": {
+        "name": "Linus",
+        "year": 2011
+    }
+}
+
+child1 = {
+    "name": "Emil",
+    "year": 2004
+}
+
+child2 = {
+    "name": "Tobias",
+    "year": 2007
+}
+
+child3 = {
+    "name": "Linus",
+    "year": 2011
+}
+
+myfamily = {
+    "child1": child1,
+    "child2": child2,
+    "child3": child3
+}
+
+print(myfamily["child2"]["name"])
+
+for x, obj in myfamily.items():
+    print(x)
+
+    for y in obj:
+        print(y + ':', obj[y])
+
+car = {"brand": "Ford", "model": "Mustang", "year": 2024}
+print(car["model"])
+car["color"] = "red"
+car.pop("brand")
+print(car)
