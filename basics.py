fa248f46290968ec9f28dd9a807bb2b33b40a5d0
April <liska.april@gmail.com>
2026-07-26 20:04:40 -0400

    Python User Input

--- basics.py
+++ basics.py
@@ -2958,3 +2958,39 @@ print(x)
 x = None
 if x is None:
     print("x is None")
+
+#Python User Input
+print("Enter your name:")
+name = "none"
+#name = input()
+print(f"Hello {name}")
+
+#name = input("Enter your name:")
+print(f"Hello {name}")
+
+fav1 = "pass"
+fav2 = "pass"
+fav3 = "pass"
+
+#name = input("Enter your name:")
+print(f"Hello {name}")
+# fav1 = input("What is your favorite animal:")
+# fav2 = input("What is your favorite color:")
+# fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+
+x = 2
+#x = input("Enter a number:")
+y = math.sqrt(float(x))
+print(f"The square root of {x} is {y}")
+
+y = True
+while y == True:
+    #x = input("Enter a number:")
+    try:
+        x = float(x)
+        y = False
+    except:
+        print("Wrong input, please try again.")
+
+print("Thank you!")
