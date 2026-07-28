b0af4d92344112ff898dcc2ee2fa0009bffecd18
April <liska.april@gmail.com>
2026-07-27 21:01:36 -0400

    Python Encapsulation

--- classes.py
+++ classes.py
@@ -504,3 +504,115 @@ f1 = Fox()
 
 for animal in (c1, f1):
     animal.sound()
+
+#Python Encapsulation
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age #new
+
+p1 = Person("Emil", 25)
+print(p1.name)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+
+    def get_age(self):
+        return self.__age
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        if age > 0:
+            self.__age = age
+        else:
+            print("Age must be positive")
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+p1.set_age(26)
+print(p1.get_age())
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.__grade = 0
+
+    def set_grade(self, grade):
+        if 0 <= grade <= 100:
+            self.__grade = grade
+        else:
+            print("Grade must be between 0 and 100")
+
+    def get_grade(self):
+        return self.__grade
+
+    def get_status(self):
+        if self.__grade >= 60:
+            return "Passed"
+        else:
+            return "Failed"
+
+student = Student("Emil")
+student.set_grade(85)
+print(student.get_grade())
+print(student.get_status())
+
+class Person:
+    def __init__(self, name, salary):
+        self.name = name
+        self._salary = salary
+
+p1 = Person("Linus", 50000)
+print(p1.name)
+print(p1._salary)
+
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def __validate(self, num):
+        if not isinstance(num, (int, float)): #new
+            return False
+        return True
+
+    def add(self, num):
+        if self.__validate(num):
+            self.result += num
+        else:
+            print("Invalid number")
+
+calc = Calculator()
+calc.add(10)
+calc.add(5)
+print(calc.result)
+calc.add("test")
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+p1 = Person("Emil", 30)
+print(p1._Person__age) #new
+
+class ScoreBoard:
+    def __init__(self, score):
+        self.__score = score
+
+    def get_score(self):
+        return self.__score
+    
+s1 = ScoreBoard(0)
+print(s1.get_score())
