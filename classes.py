412651561d6da31711fdd78eaa9753a278e9a463
April <liska.april@gmail.com>
2026-07-27 20:30:08 -0400

    Python Polymorphism

--- classes.py
+++ classes.py
@@ -421,3 +421,86 @@ class Dog(Animal):
 
 d1 = Dog("Rex")
 d1.speak()
+
+#Python Polymorphism
+x = "Hello World!"
+
+print(len(x))
+
+mytuple = ("apple", "banana", "cherry")
+
+print(len(mytuple))
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(len(thisdict))
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    def move(self):
+        print("Drive!")
+class Boat:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    def move(self):
+        print("Sail!")
+class Plane:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    def move(self):
+        print("Fly!")
+car1 = Car("Ford", "Mustang")
+boat1 = Boat("Ibiza", "Touring 20")
+plane1 = Plane("Boeing", "747")
+for x in (car1, boat1, plane1):
+    x.move() #new
+
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Move!")
+
+class Car(Vehicle):
+    pass
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sail!")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Fly!")
+
+car1 = Car("Ford", "Mustang")
+boat1 = Boat("Ibiza", "Touring 20")
+plane1 = Plane("Boeing", "747")
+
+for x in (car1, boat1, plane1):
+    print(x.brand)
+    print(x.model)
+    x.move()
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+class Fox:
+    def sound(self):
+        print("Wa-pa-pa-pa-pa-pow!")
+
+c1 = Cat()
+f1 = Fox()
+
+for animal in (c1, f1):
+    animal.sound()
