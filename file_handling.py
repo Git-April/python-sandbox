2fca44166ae92a746a33db6c4a09e01a315fb025
April <liska.april@gmail.com>
2026-07-27 21:36:49 -0400

    Python Delete Files

--- file_handling.py
+++ file_handling.py
@@ -1,4 +1,6 @@
-#Python File Handling
+import os #Python Delete Files
+
+#Python Read Files
 f = open("file_handling_demofile.txt")
 f = open("file_handling_demofile.txt", "rt")
 
@@ -26,6 +28,7 @@ with open("file_handling_demofile.txt") as f:
     for x in f:
         print(x)
 
+#Python Write/Create Files
 with open("file_handling_demofile.txt", "a") as f:
     f.write("Now the file has more content!")
 
@@ -37,3 +40,9 @@ with open("file_handling_demofile.txt", "w") as f:
 
 with open("file_handling_demofile.txt") as f:
     print(f.read())
+
+#Python Delete Files
+if os.path.exists("demofile.txt"):
+    os.remove("demofile.txt")
+else:
+    print("The file does not exist")
