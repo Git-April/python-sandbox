08542d7db360a35ea290d7496df1b14fbd4b0f16
April <liska.april@gmail.com>
2026-07-30 22:28:03 -0400

    NumPy Data Types

--- numpy_basics.py
+++ numpy_basics.py
@@ -112,3 +112,33 @@ print(arr[0:2, 2])
 
 arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 print(arr[0:2, 1:4])
+
+#NumPy Data Types
+arr = np.array([1, 2, 3, 4])
+print(arr.dtype)
+
+arr = np.array(['apple', 'banana', 'cherry'])
+print(arr.dtype)
+
+arr = np.array([1, 2, 3, 4], dtype='S')
+print(arr)
+print(arr.dtype)
+
+arr = np.array([1, 2, 3, 4], dtype="i4")
+print(arr)
+print(arr.dtype)
+
+arr = np.array([1.1, 2.1, 3.1])
+newarr = arr.astype('i')
+print(newarr)
+print(newarr.dtype)
+
+arr = np.array([1.1, 2.1, 3.1])
+newarr = arr.astype(int)
+print(newarr)
+print(newarr.dtype)
+
+arr = np.array([1, 0, 3])
+newarr = arr.astype(bool)
+print(newarr)
+print(newarr.dtype)
