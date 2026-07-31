d756956e140d5199ea1e95bff2133709d4efd553
April <liska.april@gmail.com>
2026-07-30 22:15:51 -0400

    NumPy Array Slicing

--- numpy_basics.py
+++ numpy_basics.py
@@ -84,3 +84,31 @@ print(arr[0, 1, 2])
 
 arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 print('Last element from 2nd dim: ', arr[1, -1])
+
+#NumPy Array Slicing
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[4:])
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[:4])
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[-3:-1])
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5:2])
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[::2])
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[1, 1:4])
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[0:2, 2])
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[0:2, 1:4])
