e557e3d33a9e6224557fc4100cf06acacd552786
April <liska.april@gmail.com>
2026-07-30 22:31:24 -0400

    NumPy Copy vs View

--- numpy_basics.py
+++ numpy_basics.py
@@ -142,3 +142,30 @@ arr = np.array([1, 0, 3])
 newarr = arr.astype(bool)
 print(newarr)
 print(newarr.dtype)
+
+#NumPy Copy vs View
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+arr[0] = 42
+
+print(arr)
+print(x)
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+arr[0] = 42
+
+print(arr)
+print(x)
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+x[0] = 31
+
+arr = np.array([1, 2, 3, 4, 5])
+
+x = arr.copy()
+y = arr.view()
+
+print(x.base)
+print(y.base)
