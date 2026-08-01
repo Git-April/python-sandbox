2789bba2fd369f006ed3227f2670fe1aeed51874
April <liska.april@gmail.com>
2026-08-01 16:19:03 -0400

    Pareto Distribution

--- numpy_random.py
+++ numpy_random.py
@@ -143,3 +143,10 @@ print(x)
 
 # sns.displot(random.rayleigh(size=1000), kind="kde")
 # plt.show()
+
+#Pareto Distribution
+x = random.pareto(a=2, size=(2, 3))
+print(x)
+
+# sns.displot(random.pareto(a=2, size=1000))
+# plt.show()
