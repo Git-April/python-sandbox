5af34b49326b965cc5933722a4ba79ae33b4527a
April <liska.april@gmail.com>
2026-08-01 15:08:19 -0400

    Data Distribution

--- numpy_random.py
+++ numpy_random.py
@@ -24,3 +24,10 @@ print(x)
 
 x = random.choice([3, 5, 7, 9], size=(3, 5))
 print(x)
+
+#Data Distribution
+x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6 ,0.0], size=(100))
+print(x)
+
+x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(3, 5))
+print(x)
