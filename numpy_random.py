98d19af11ca9b0b358630c721afc41fa6d963385
April <liska.april@gmail.com>
2026-08-01 16:15:03 -0400

    Chi Square Distribution

--- numpy_random.py
+++ numpy_random.py
@@ -127,5 +127,12 @@ print(x)
 x = random.exponential(scale=2, size=(2, 3))
 print(x)
 
-sns.displot(random.exponential(size=1000), kind="kde")
-plt.show()
+# sns.displot(random.exponential(size=1000), kind="kde")
+# plt.show()
+
+#Chi Square Distribution
+x = random.chisquare(df=2, size=(2, 3))
+print(x)
+
+# sns.displot(random.chisquare(df=1, size=1000), kind="kde")
+# plt.show()
