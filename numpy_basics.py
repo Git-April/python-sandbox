ca5925fa4113368eed1022467ca8e9046255d39b
April <liska.april@gmail.com>
2026-07-31 23:30:42 -0400

    NumPy Array Join

--- numpy_basics.py
+++ numpy_basics.py
@@ -252,3 +252,41 @@ arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
 for idx, x in np.ndenumerate(arr):
   print(idx, x)
+
+#NumPy Array Join
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.concatenate((arr1, arr2))
+
+print(arr)
+
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+
+arr = np.concatenate((arr1, arr2), axis=1)
+print(arr)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.stack((arr1, arr2), axis=1)
+print(arr)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.hstack((arr1, arr2))
+print(arr)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.vstack((arr1, arr2))
+print(arr)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.dstack((arr1, arr2))
+print(arr)
