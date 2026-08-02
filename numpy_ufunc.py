16c7fc84bee5137347b8ee894c21cf78430dab71
April <liska.april@gmail.com>
2026-08-01 22:20:03 -0400

    ufunc Set Operations

--- numpy_ufunc.py
+++ numpy_ufunc.py
@@ -219,3 +219,28 @@ print(x)
 arr = np.array([0.1, 0.2, 0.5])
 x = np.arctanh(arr)
 print(x)
+
+#ufunc Set Operations
+arr = np.array([1, 1, 1, 2, 3, 4, 5, 5, 6, 7])
+x = np.unique(arr)
+print(x)
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([3, 4, 5, 6])
+newarr = np.union1d(arr1, arr2)
+print(newarr)
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([3, 4, 5, 6])
+newarr = np.intersect1d(arr1, arr2, assume_unique=True)
+print(newarr)
+
+set1 = np.array([1, 2, 3, 4])
+set2 = np.array([3, 4, 5, 6])
+newarr = np.setdiff1d(set1, set2, assume_unique=True)
+print(newarr)
+
+set1 = np.array([1, 2, 3, 4])
+set2 = np.array([3, 4, 5, 6])
+newarr = np.setxor1d(set1, set2, assume_unique=True)
+print(newarr)
