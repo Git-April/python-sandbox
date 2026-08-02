3185d3341d6bd2fa4c9e10a1d02461c8d22141f9
April <liska.april@gmail.com>
2026-08-01 21:45:42 -0400

    ufunc Rounding Decimals

--- numpy_ufunc.py
+++ numpy_ufunc.py
@@ -77,3 +77,16 @@ print(newarr)
 arr = np.array([-1, -2, 1, 2, 3, -4])
 newarr = np.absolute(arr)
 print(newarr)
+
+#ufunc Rounding Decimals
+arr = np.trunc([-3.1666, 3.6667])
+print(arr)
+
+arr = np.around(3.1666, 2)
+print(arr)
+
+arr = np.floor([-3.1666, 3.6667])
+print(arr)
+
+arr = np.ceil([-3.1666, 3.6667])
+print(arr)
