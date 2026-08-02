6dd39b062f028de42e8e11a28371417ab01cf99f
April <liska.april@gmail.com>
2026-08-02 19:04:23 -0400

    Pandas Series

--- pandas_basics.py
+++ pandas_basics.py
@@ -21,3 +21,31 @@ myvar = pd.DataFrame(mydataset)
 print(myvar)
 
 print(pd.__version__)
+
+#Pandas Series
+a = [1, 7, 2]
+myvar = pd.Series(a)
+print(myvar)
+
+print(myvar[0])
+
+a = [1, 7, 2]
+myvar = pd.Series(a, index = ["x", "y", "z"])
+print(myvar)
+
+print(myvar["y"])
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar = pd.Series(calories)
+print(myvar)
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar = pd.Series(calories, index = ["day1", "day2"])
+print(myvar)
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+myvar = pd.DataFrame(data)
+print(myvar)
