67094d7a53de0df8b3227da76047935eebaf1884
April <liska.april@gmail.com>
2026-08-01 21:30:35 -0400

    ufunc Create Function

--- numpy_ufunc.py
+++ numpy_ufunc.py
@@ -13,3 +13,20 @@ x = [1, 2, 3, 4]
 y = [4, 5, 6, 7]
 z = np.add(x, y)
 print(z)
+
+#ufunc Create Function
+def myadd(x, y):
+  return x+y
+
+myadd = np.frompyfunc(myadd, 2, 1)
+
+print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))
+
+print(type(np.add))
+
+print(type(np.concatenate))
+
+if type(np.add) == np.ufunc:
+  print('add is ufunc')
+else:
+  print('add is not ufunc')
