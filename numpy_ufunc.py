a4916c07cc59178e1da5c71708295ae54ec12602
April <liska.april@gmail.com>
2026-08-01 22:14:33 -0400

    ufunc Hyperbolic

--- numpy_ufunc.py
+++ numpy_ufunc.py
@@ -204,3 +204,18 @@ base = 3
 perp = 4
 x = np.hypot(base, perp)
 print(x)
+
+#ufunc Hyperbolic
+x = np.sinh(np.pi/2)
+print(x)
+
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+x = np.cosh(arr)
+print(x)
+
+x = np.arcsinh(1.0)
+print(x)
+
+arr = np.array([0.1, 0.2, 0.5])
+x = np.arctanh(arr)
+print(x)
