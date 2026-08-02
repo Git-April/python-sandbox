d390e669448193c87b46b29835dba6d0b29bc787
April <liska.april@gmail.com>
2026-08-01 21:52:12 -0400

    ufunc Summations

--- numpy_ufunc.py
+++ numpy_ufunc.py
@@ -104,3 +104,23 @@ print(np.log(arr))
 
 nplog = np.frompyfunc(log, 2, 1)
 print(nplog(100, 15))
+
+#ufunc Summations
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+newarr = np.add(arr1, arr2)
+print(newarr)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+newarr = np.sum([arr1, arr2])
+print(newarr)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+newarr = np.sum([arr1, arr2], axis=1)
+print(newarr)
+
+arr = np.array([1, 2, 3])
+newarr = np.cumsum(arr)
+print(newarr)
