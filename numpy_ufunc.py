d6cb2c85ed8142189971b0e6a1cdacb15c6d58ed
April <liska.april@gmail.com>
2026-08-01 21:54:42 -0400

    ufunc Products

--- numpy_ufunc.py
+++ numpy_ufunc.py
@@ -124,3 +124,22 @@ print(newarr)
 arr = np.array([1, 2, 3])
 newarr = np.cumsum(arr)
 print(newarr)
+
+#ufunc Products
+arr = np.array([1, 2, 3, 4])
+x = np.prod(arr)
+print(x)
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+x = np.prod([arr1, arr2])
+print(x)
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+newarr = np.prod([arr1, arr2], axis=1)
+print(newarr)
+
+arr = np.array([5, 6, 7, 8])
+newarr = np.cumprod(arr)
+print(newarr)
