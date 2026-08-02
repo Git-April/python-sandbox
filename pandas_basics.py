0f436ac78dab304142b0a6b62799fd17ec407dc1
April <liska.april@gmail.com>
2026-08-02 18:52:55 -0400

    Pandas Getting Started

--- pandas_basics.py
+++ pandas_basics.py
@@ -4,3 +4,20 @@ import pandas as pd #Pandas HOME
 df = pd.read_csv('pandas_basics_data.csv')
 
 print(df.to_string())
+
+#Pandas Getting Started
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+myvar = pd.DataFrame(mydataset)
+print(myvar)
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+myvar = pd.DataFrame(mydataset)
+print(myvar)
+
+print(pd.__version__)
