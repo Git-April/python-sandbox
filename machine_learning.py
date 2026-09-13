1f3921e11ab3afec8ff4cdb95ffd9183babaedf7
April <liska.april@gmail.com>
2026-09-12 22:20:29 -0400

    continue python machine learning tutorial

--- machine_learning.py
+++ machine_learning.py
@@ -1,5 +1,6 @@
 import numpy
 from scipy import stats
+import matplotlib.pyplot as plt
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
@@ -20,3 +21,13 @@ print(x)
 
 x = numpy.percentile(speed, 0.75)
 print(x)
+
+x = numpy.random.uniform(0.0, 5.0, 250)
+print(x)
+
+plt.hist(x, 5)
+
+x = numpy.random.uniform(0.0, 5.0, 100000)
+
+plt.hist(x, 5)
+plt.show()
