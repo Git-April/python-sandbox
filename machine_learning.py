30aea8e70e09cd914996956919799054f124419d
April <liska.april@gmail.com>
2026-09-13 22:28:46 -0400

    continue python machine learning

--- machine_learning.py
+++ machine_learning.py
@@ -25,9 +25,15 @@ print(x)
 x = numpy.random.uniform(0.0, 5.0, 250)
 print(x)
 
-plt.hist(x, 5)
+# plt.hist(x, 5)
 
 x = numpy.random.uniform(0.0, 5.0, 100000)
 
-plt.hist(x, 5)
+# plt.hist(x, 5)
+
+x = numpy.random.normal(5.0, 1.0, 100000)
+
+# plt.hist(x, 100)
+
+
 plt.show()
