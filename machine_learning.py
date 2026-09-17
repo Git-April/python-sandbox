59a0b2423f92f16db1c20842c94ee95a62afc3e2
April <liska.april@gmail.com>
2026-09-16 20:52:22 -0400

    continue machine learning

--- machine_learning.py
+++ machine_learning.py
@@ -1,6 +1,7 @@
 import numpy
 from scipy import stats
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
@@ -69,5 +70,32 @@ mymodel = list(map(myfunc, x))
 # plt.plot(x, mymodel)
 
 print(r)
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+myline = numpy.linspace(1, 22, 100)
+
+# plt.scatter(x, y)
+# plt.plot(myline, mymodel(myline))
+
+print(r2_score(y, mymodel(x)))
+
+speed = mymodel(17)
+print(speed)
+
+x = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
+y = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15]
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+myline = numpy.linspace(2, 95, 100)
+
+# plt.scatter(x, y)
+# plt.plot(myline, mymodel(myline))
+
+print(r2_score(y, mymodel(x)))
 
 plt.show()
