644972898ea0e2bd544f452426f900f63685e1db
April <liska.april@gmail.com>
2026-09-24 23:17:41 -0400

    continue machine learning

--- machine_learning.py
+++ machine_learning.py
@@ -3,7 +3,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 import pandas
-from sklearn import linear_model, tree
+from sklearn import linear_model, tree, metrics
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 
@@ -191,7 +191,25 @@ print(y)
 dtree = DecisionTreeClassifier()
 dtree = dtree.fit(X, y)
 
-tree.plot_tree(dtree, feature_names=features)
+# tree.plot_tree(dtree, feature_names=features)
 
 print(dtree.predict([[40, 10, 7, 1]]))
 print(dtree.predict([[40, 10, 6, 1]]))
+
+actual = numpy.random.binomial(1, 0.9, size = 1000)
+predicted = numpy.random.binomial(1, 0.9, size = 1000)
+
+confusion_matrix = metrics.confusion_matrix(actual, predicted)
+
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
+
+# cm_display.plot()
+# plt.show()
+
+Accuracy = metrics.accuracy_score(actual, predicted)
+Precision = metrics.precision_score(actual, predicted)
+Sensitivity_recall = metrics.recall_score(actual, predicted)
+Specificity = metrics.recall_score(actual, predicted, pos_label=0)
+F1_score = metrics.f1_score(actual, predicted)
+
+print({"Accuracy":Accuracy, "Precision":Precision, "Sensitivity_recall":Sensitivity_recall, "Specificity":Specificity, "F1_score":F1_score})
